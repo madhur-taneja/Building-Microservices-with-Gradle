bcc043a7914a3f67313567869905e624d9a93eae
madhur_taneja <tanejamadhur96@gmail.com>
2019-10-14 17:37:49 +0530

    Adding an API with hardcoded data

--- movie-catalog-service/src/main/java/io/trpspringmicroservices/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/trpspringmicroservices/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,13 +1,19 @@
 package io.trpspringmicroservices.moviecatalogservice.resources;
 
 import io.trpspringmicroservices.moviecatalogservice.models.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Collections;
 import java.util.List;
 
+@RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource
 {
-    public List<CatalogItem> getCatalog(String userId) {
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         return Collections.singletonList(
                 new CatalogItem("Snowden", "I know you're watching", 5)
         );
